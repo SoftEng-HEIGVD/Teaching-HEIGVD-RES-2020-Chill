6735b56fe473e133798c53b4e0e66b04fe3c7633
David Dupraz <duprazdavid@gmail.com>
2020-02-22 17:23:46 +0100

    correction

--- src/test/java/ch/heigvd/res/chill/domain/wasadigi/DiesIraeTest.java
+++ src/test/java/ch/heigvd/res/chill/domain/wasadigi/DiesIraeTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DiesIraeTest
 {
     @Test
-    void thePriceAndNameForLaCorneShouldBeCorrect()
+    void thePriceAndNameForDiesIraeShouldBeCorrect()
     {
         DiesIrae beer = new DiesIrae();
         assertEquals(beer.getName(), DiesIrae.NAME);
