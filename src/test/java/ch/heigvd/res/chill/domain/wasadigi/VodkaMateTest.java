5e2e70e65a20a78060b6916d24fce7d4cc2c1b97
Robin Cuénoud <robin.cuenoud@heig-vd.ch>
2020-02-24 21:06:02 +0100

    added Vodka Mate

---
+++ src/test/java/ch/heigvd/res/chill/domain/wasadigi/VodkaMateTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.res.chill.domain.wasadigi;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class VodkaMateTest {
+
+    @Test
+    void thePriceAndNameForVodkaMateShouldBeCorrect() {
+        VodkaMate beer = new VodkaMate();
+        assertEquals(beer.getName(), VodkaMate.NAME);
+        assertEquals(beer.getPrice(), VodkaMate.PRICE);
+    }
+
+    @Test
+    void aBartenderShouldAcceptAnOrderForVodkaMate() {
+        Bartender jane = new Bartender();
+        String productName = "ch.heigvd.res.chill.domain.wasadigi.VodkaMate";
+        OrderRequest request = new OrderRequest(3, productName);
+        OrderResponse response = jane.order(request);
+        BigDecimal expectedTotalPrice = VodkaMate.PRICE.multiply(new BigDecimal(3));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+
+}
